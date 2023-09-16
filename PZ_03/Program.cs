e4e21dd286563f42ecb0152b23fe242ff0d027b1
Nina-Nishkur-2pk1 <ninaalex228811@gmail.com>
2023-09-16 14:39:55 +0500

    добавлена 3 практика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Выберите тип аккаунта: ");
+            string typeAccount = Console.ReadLine().ToLower();
+            string login, password, email, bankData;
+            switch (typeAccount)
+            {
+                case "временный":
+                    Console.Write("Введите логин: ");
+                    login = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    password = Console.ReadLine();
+                    break;
+                case "стандартный":
+                    Console.Write("Введите логин: ");
+                    login = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    password = Console.ReadLine();
+                    Console.Write("Введите привязанную почту: ");
+                    email = Console.ReadLine();
+                    break;
+                case "золотой":
+                    Console.Write("Введите логин: ");
+                    login = Console.ReadLine();
+                    Console.Write("Введите пароль: ");
+                    password = Console.ReadLine();
+                    Console.Write("Введите привязанную почту: ");
+                    email = Console.ReadLine();
+                    Console.Write("Введите номер банковской карты: ");
+                    bankData = Console.ReadLine();
+                    break; 
+             default:
+                    Console.WriteLine("Вы выбрали неверный тип аккаунта!");
+                    break;
+            }
+        }
+    }
+}
