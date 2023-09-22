ef552e134883e1dfa9c2273393b3a4198a778c9f
Nina-Nishkur-2pk1 <ninaalex228811@gmail.com>
2023-09-22 16:24:57 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double h = 0.5;
+
+            Console.WriteLine("x       y");
+            Console.WriteLine("_________________");
+
+            while (x <= 4)
+            {
+                double y = x + 3 * Math.Pow(x, 2) - 2.5 * Math.Pow(x, 3) + 0.75 * Math.Pow(x, 4);
+                Console.WriteLine($"{x}       {y}");
+                x += h;
+            }
+
+        }
+    }
+}
