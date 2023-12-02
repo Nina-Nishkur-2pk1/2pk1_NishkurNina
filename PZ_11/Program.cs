7dfcc88693d49e27f503f4f64a07f57a5dffb372
Nina-Nishkur-2pk1 <ninaalex228811@gmail.com>
2023-12-02 14:47:37 +0500

    добавлена 11 практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+
+
+        static void Swap(ref double x, ref double y)
+        {
+            double num = x; 
+            x = y; 
+            y = num; 
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите значение переменной A: ");
+            double A = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение переменной B: ");
+            double B = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение переменной C: ");
+            double C = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение переменной D: ");
+            double D = double.Parse(Console.ReadLine());
+
+            Swap(ref A, ref B); // меняем содержимое переменных A и B
+            Swap(ref C, ref D); // меняем содержимое переменных C и D
+            Swap(ref B, ref C); // меняем содержимое переменных B и C
+
+            Console.WriteLine("Новые значения переменных: ");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("C: " + C);
+            Console.WriteLine("D: " + D);
+        }
+    }
+}
