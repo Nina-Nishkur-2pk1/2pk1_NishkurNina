6ab10a1c6fa915c863140486e48f7de83998e59c
Nina-Nishkur-2pk1 <ninaalex228811@gmail.com>
2023-12-07 15:10:32 +0500

    добавлена 14 практика

---
+++ PZ_14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Путь к файлу
+        string filePath = @"C:\Users\Пк\File\newfile.txt";
+
+        try
+        {
+            // Чтение всех строк из файла
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Сортировка символов в каждой строке
+            string[] sortedLines = lines.Select(line => new string(line.OrderBy(c => c).ToArray())).ToArray();
+
+            // Перезапись файла с отсортированными строками
+            File.WriteAllLines(filePath, sortedLines);
+
+            Console.WriteLine("Сортировка завершена.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
+        
