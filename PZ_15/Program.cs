8ddb2bf0aa2b91fea19ab213cf8957cf78c0f765
Nina-Nishkur-2pk1 <ninaalex228811@gmail.com>
2023-12-18 22:19:40 +0500

    добавлена 15 практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            {
+                Console.WriteLine("Введите полный путь к каталогу:");
+                string directoryPath = Console.ReadLine();
+
+                if (Directory.Exists(directoryPath)) // Проверяем существование каталога
+                {
+                    string[] files = Directory.GetFiles(directoryPath); // Получаем список файлов в каталоге
+
+                    foreach (string file in files)
+                    {
+                        try
+                        {
+                            using (StreamWriter writer = File.AppendText(file)) // Открываем файл для дозаписи
+                            {
+                                writer.WriteLine($"Файл {file} обновлен. Время создания: {File.GetCreationTime(file)}");
+                            }
+                            Console.WriteLine($"Файл {file} обновлен"); // Выводим сообщение об обновлении файла
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Ошибка при обновлении файла {file}: {e.Message}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указанный каталог не существует");
+                }
+            }
+        }
+
+
+      
+        }
+}
